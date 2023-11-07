77cb3bd536168348f22bb59f7c993f029f245241
nikita-2697 <71069466+nikita-2697@users.noreply.github.com>
2023-11-07 15:45:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import page2 as p2
 import plotly.express as px
-# import prophet.Prophet as p
+import prophet
+import prophet.Prophet as p
 # import Prophet from p
 st.write("HIII")
 header = st.container()

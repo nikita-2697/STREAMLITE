dbfd31db35e6aa03cf6d8a46ee027e3fdefca16c
nikita-2697 <71069466+nikita-2697@users.noreply.github.com>
2023-11-07 14:00:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import pandas as pd
+import page2 as p2
 st.write("HIII")
 header = st.container()
 with header:
   st.title("Hii welcome")
   data = pd.read_csv("data.csv")
   st.write(data.head())
+  p2.showthis()

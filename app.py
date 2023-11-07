5a575d140a6d17d51f84d2f0a3800d70a342d670
nikita-2697 <71069466+nikita-2697@users.noreply.github.com>
2023-11-07 12:33:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 st.write("HIII")
 header = st.container()
 with header:
   st.title("Hii welcome")
+  data = st.read_csv("data.csv")
+  st.write(data.head)

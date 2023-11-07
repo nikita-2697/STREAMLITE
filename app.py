4238a12c0de5d766d78feb21bda54cf3a309f30c
nikita-2697 <71069466+nikita-2697@users.noreply.github.com>
2023-11-07 15:32:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import page2 as p2
 import plotly.express as px
+import prophet 
 st.write("HIII")
 header = st.container()
 with header:
@@ -11,3 +12,5 @@ with header:
   abc= data['14.14'].head()
   val = st.selectbox("AccountID", abc)
   p2.showthis(val)
+
+# st.set_page_config(page_title = "")
